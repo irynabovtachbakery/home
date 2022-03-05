50f93f0ef8d947ac7b68bc943f4557782f53389a
irynabovtachbakery <99831841+irynabovtachbakery@users.noreply.github.com>
2022-03-05 17:23:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+function SmoothVerticalScrolling(e, time, where) {
+    var eTop = e.getBoundingClientRect().top;
+    var eAmt = eTop / 100;
+    var curTime = 0;
+    while (curTime <= time) {
+        window.setTimeout(SVS_B, curTime, eAmt, where);
+        curTime += time / 100;
+    }
+}
+
+function SVS_B(eAmt, where) {
+    if(where == "center" || where == "")
+        window.scrollBy(0, eAmt / 2);
+    if (where == "top")
+        window.scrollBy(0, eAmt);
+}
+
+
+
+
+
+
+
+
+
+
 //DETAILS-TEXT_______________________________________
 
 const btnDetailStart = document.querySelector('.start-btn');
@@ -241,4 +267,8 @@ toUp.addEventListener('click', function(){
 
       
   })
+
+
+
+
 
